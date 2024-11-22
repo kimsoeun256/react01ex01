b24f9e5d7996fad8ef6d577ec0d553aca14ffea3
kimsoeun256 <gimsoeun256@gmail.com>
2024-11-22 22:27:42 +0900

    첫 번째 커밋

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [profile, setProfile] = useState([
+    { src: "/thumb1.jpg", name: "Mario", score: 5, bg: "url('/bg1.jpg')" },
+    { src: "/thumb2.jpg", name: "Zelda", score: 4, bg: "url('/bg2.jpg')" },
+    { src: "/thumb3.jpg", name: "Pokemon", score: 3, bg: "url('/bg3.jpg')" },
+  ])
+
+  const handleScoreClick = (i) => {
+    let copy = [...profile];
+    copy[i].score = copy[i].score + 1;
+    const sortedCopy = copy.sort((a, b)=> b.score - a.score);
+    setProfile(sortedCopy);
+  }
+
+  return (
+    <div
+      className="w-full h-full flex flex-col bg-slate-400 bg-custom bg-cover"
+      style={{ background: process.env.PUBLIC_URL + profile[0].bg }}
+    >
+      <h2 className="text-[100px] text-white text-center pt-10">
+        Nintendo Score
+      </h2>
+      <div className="flex flex-row h-full px-20 justify-around items-center gap-10">
+        {
+          profile.map((v, i)=> {
+            return (   
+              <div className="text-center text-white mx-10">
+                <img
+                  className="rounded-3xl"
+                  src={process.env.PUBLIC_URL + profile[i].src}
+                  alt=""
+                />
+                <h1 className="text-[80px]">{profile[i].name}</h1>
+                <h4 className="text-[40px]" onClick={()=>handleScoreClick(i)}>
+                  Score<span className="pl-4">{profile[i].score}</span>
+                </h4>
+              </div>
+            )
+          })
+        }
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
